f63e6f947892dd7860601aee1caf5eae29625a90
Espen A. Kristiansen <post@espenak.net>
2015-10-26 17:00:01 +0100

    Changed the dump to json script into dump to sql.

--- ievv_opensource/ievvtasks_development/management/commands/ievvtasks_recreate_devdb.py
+++ ievv_opensource/ievvtasks_development/management/commands/ievvtasks_recreate_devdb.py
@@ -1,19 +1,21 @@
+import os
 from django.core import management
 from django.core.management.base import BaseCommand
 
-from ievv_opensource.ievvtasks_development.management.commands.ievvtasks_dump_db_as_json import \
+from ievv_opensource.ievvtasks_development.management.commands.ievvtasks_dump_db_as_sql import \
     get_dumpdata_filepath
 
 
 class Command(BaseCommand):
     help = 'Recreate the database using django_dbdev migrate the database ' \
            'and load the json data dump created with ' \
-           'ievvtasks_dump_devdb_as_json.'
+           'ievvtasks_dump_devdb_as_sql.'
 
     def handle(self, *args, **options):
         management.call_command('ievvtasks_remove_sorl_cache_media')
         management.call_command('dbdev_reinit')
-        management.call_command('migrate')
         dumpdatafile = get_dumpdata_filepath()
-        self.stdout.write('Loading data from {}.'.format(dumpdatafile))
-        management.call_command('loaddata', dumpdatafile)
+        if os.path.exists(dumpdatafile):
+            self.stdout.write('Loading data from {}.'.format(dumpdatafile))
+            management.call_command('dbdev_loaddump', dumpdatafile)
+        management.call_command('migrate')
