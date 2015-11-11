7c0aa72e6a84488e4ed5d159a33bd5829ef20690
j3sus89 <jesus.cardenasgonzalez@gmail.com>
2015-11-11 10:10:39 +0100

    ievv_eventframework: more tests added

--- ievv_opensource/ievv_eventframework/models.py
+++ ievv_opensource/ievv_eventframework/models.py
@@ -51,9 +51,9 @@ class Event(models.Model):
         This functions gets the data from the text string and makes an event.
         """
         parsed_text_area = markdown.markdown(text_area, entensions=[MyExtension()])
-        print(parsed_text_area)
+        print("After: "+parsed_text_area)
         parsed_html = BeautifulSoup(parsed_text_area, "html.parser")
-        print("LO QUE DEVUELVE: "+str(parsed_html))
+        print("Before: "+str(parsed_html))
         ev = Event.create(parsed_html.find('h1').text,
                           parsed_html.find_all('p')[1].text,
                           parsed_html.find('date').text,
